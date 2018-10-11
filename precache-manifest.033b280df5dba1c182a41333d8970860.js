56c02a737c18ec3bc791eefd9ee1b4cbec0548cd
ceduardogodoi <carloseduardoalvesgodoi@hotmail.com>
2018-10-11 12:58:05 -0300

    Updates

---
+++ precache-manifest.033b280df5dba1c182a41333d8970860.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "d835663e21fa4a8ed0d5",
+    "url": "/react-tutorial/static/js/main.d835663e.chunk.js"
+  },
+  {
+    "revision": "855276873adeae1ea0f1",
+    "url": "/react-tutorial/static/js/1.85527687.chunk.js"
+  },
+  {
+    "revision": "d835663e21fa4a8ed0d5",
+    "url": "/react-tutorial/static/css/main.c3fec1ec.chunk.css"
+  },
+  {
+    "revision": "4be5d3829bd507ca2ab3128bcf9721f2",
+    "url": "/react-tutorial/index.html"
+  }
+];
